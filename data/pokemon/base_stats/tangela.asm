23e2bc40d32f030e8413fdba4d286d0318d3eded
peterjtalen <42253616+peterjtalen@users.noreply.github.com>
2020-01-17 15:30:24 -0700

    Update tangela.asm

--- data/pokemon/base_stats/tangela.asm
+++ data/pokemon/base_stats/tangela.asm
@@ -3,7 +3,7 @@
 	db  65,  55, 115,  60, 100,  55
 	;   hp  atk  def  spd  sat  sdf
 
-	db GRASS, GRASS ; type
+	db GRASS, DARK ; type
 	db 45 ; catch rate
 	db 166 ; base exp
 	db NO_ITEM, NO_ITEM ; items
