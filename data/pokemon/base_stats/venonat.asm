e0b2d7ff902c839c7a71a4c7522d0466de3e535b
peterjtalen <42253616+peterjtalen@users.noreply.github.com>
2020-01-17 15:41:09 -0700

    Update venonat.asm

--- data/pokemon/base_stats/venonat.asm
+++ data/pokemon/base_stats/venonat.asm
@@ -3,7 +3,7 @@
 	db  60,  55,  50,  45,  40,  55
 	;   hp  atk  def  spd  sat  sdf
 
-	db BUG, POISON ; type
+	db BUG, PSYCHIC ; type
 	db 190 ; catch rate
 	db 75 ; base exp
 	db NO_ITEM, NO_ITEM ; items
