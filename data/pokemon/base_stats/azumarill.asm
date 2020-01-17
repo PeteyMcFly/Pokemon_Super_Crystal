8c5125a8f4ce4eacc10abb338b160111f6d5bc26
peterjtalen <42253616+peterjtalen@users.noreply.github.com>
2020-01-17 10:32:04 -0700

    Update azumarill.asm

--- data/pokemon/base_stats/azumarill.asm
+++ data/pokemon/base_stats/azumarill.asm
@@ -1,9 +1,9 @@
 	db AZUMARILL ; 184
 
-	db 100,  50,  80,  50,  50,  80
+	db 100,  50,  80,  50,  70,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, WATER ; type
+	db WATER, NORMAL ; type
 	db 75 ; catch rate
 	db 153 ; base exp
 	db NO_ITEM, NO_ITEM ; items
