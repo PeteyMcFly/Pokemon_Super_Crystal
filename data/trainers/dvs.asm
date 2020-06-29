e53b1bc8f6850978aa5c93ca50f16f6a1719b6cb
peterjtalen <42253616+peterjtalen@users.noreply.github.com>
2020-06-29 14:15:08 -0600

    Update dvs.asm
    Updated DV's of bosses to try and make their core/top pokemon always match the gender of the trainer given the new Gender calc

--- data/trainers/dvs.asm
+++ data/trainers/dvs.asm
@@ -1,32 +1,32 @@
 TrainerClassDVs:
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	;  atk,def,spd,spc
-	dn  9, 10,  8,  7 ; FALKNER
+	dn  9,  9,  8,  7 ; FALKNER
 	dn  9,  9,  9,  8 ; WHITNEY
 	dn  9,  8,  10,  8 ; BUGSY
 	dn  9,  8,  9,  9 ; MORTY
 	dn 11, 11, 11, 8 ; PRYCE
 	dn  9,  11,  8,  8 ; JASMINE
 	dn 11,  9,  8,  7 ; CHUCK
-	dn 11, 12, 13, 13 ; CLAIR
+	dn 11, 12, 12, 13 ; CLAIR
 	dn 13, 13, 13, 13 ; RIVAL1
 	dn  9,  8,  8,  8 ; POKEMON_PROF
-	dn 13, 12, 13, 13 ; WILL
+	dn 11, 12, 13, 13 ; WILL
 	dn 13, 12, 13, 13 ; CAL
-	dn 13, 12, 13, 13 ; BRUNO
+	dn 13, 12, 12, 13 ; BRUNO
 	dn 11, 13, 13, 15 ; KAREN
 	dn 13, 12, 13, 13 ; KOGA
-	dn 13, 13, 13, 13 ; CHAMPION
-	dn  9,  11,  8,  8 ; BROCK
+	dn 13, 13, 12, 13 ; CHAMPION
+	dn 11,  11,  10,  7 ; BROCK
 	dn  7,  8,  11,  11 ; MISTY
-	dn  9,  8,  13,  9 ; LT_SURGE
+	dn 11,  8,  13,  9 ; LT_SURGE
 	dn  9,  8,  8,  8 ; SCIENTIST
-	dn  7,  10,  9, 11 ; ERIKA
+	dn  7,  11, 11, 11 ; ERIKA
 	dn  9,  8,  8,  8 ; YOUNGSTER
 	dn  9,  8,  8,  8 ; SCHOOLBOY
 	dn  9,  8,  8,  8 ; BIRD_KEEPER
 	dn  5,  8,  8,  8 ; LASS
-	dn  9,  8,  8,  8 ; JANINE
+	dn  9,  8,  9,  8 ; JANINE
 	dn 13,  8, 12,  8 ; COOLTRAINERM
 	dn  7, 12, 12,  8 ; COOLTRAINERF
 	dn  6,  9, 12,  8 ; BEAUTY
@@ -64,7 +64,7 @@ TrainerClassDVs:
 	dn  6,  8, 10,  8 ; TWINS
 	dn  6, 13,  8,  8 ; POKEFANF
 	dn 15, 13, 13, 14 ; RED
-	dn 15, 13, 13, 13 ; BLUE
+	dn 15, 12, 13, 13 ; BLUE
 	dn  9,  8,  8,  8 ; OFFICER
 	dn  7, 14, 10,  8 ; GRUNTF
 	dn  9,  8,  8,  8 ; MYSTICALMAN
