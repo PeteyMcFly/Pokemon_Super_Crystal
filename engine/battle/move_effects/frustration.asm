b54187089c560daf7f68be8b34b4d8ac29fc335e
Nolan Varani <landesherr@users.noreply.github.com>
2023-07-29 15:52:54 -0700

    Update Frustration to behave like Gen 3 Facade

--- engine/battle/move_effects/frustration.asm
+++ engine/battle/move_effects/frustration.asm
@@ -1,27 +1,8 @@
 BattleCommand_FrustrationPower:
 ; frustrationpower
-
-	push bc
-	ld hl, wBattleMonHappiness
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .got_happiness
-	ld hl, wEnemyMonHappiness
-.got_happiness
-	ld a, $ff
-	sub [hl]
-	ldh [hMultiplicand + 2], a
-	xor a
-	ldh [hMultiplicand + 0], a
-	ldh [hMultiplicand + 1], a
-	ld a, 10
-	ldh [hMultiplier], a
-	call Multiply
-	ld a, 25
-	ldh [hDivisor], a
-	ld b, 4
-	call Divide
-	ldh a, [hQuotient + 3]
-	ld d, a
-	pop bc
-	ret
+; Double damage if user is burned, poisoned, or paralyzed
+	ld a, BATTLE_VARS_STATUS
+	call GetBattleVar
+	and (1 << BRN) | (1 << PSN) | (1 << PAR)
+	ret z
+	jp DoubleDamage
