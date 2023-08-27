8289e8faf670cf56a3d18e5e90af14421826657e
Nolan Varani <landesherr@users.noreply.github.com>
2023-08-27 01:37:53 -0700

    Revert "Focus energy always crits on high-crit moves"
    Too OP on the machop series
    
    This reverts commit 4645e534974d065ee3516aeff87af24bf296cfe1.

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1212,19 +1212,9 @@ BattleCommand_Critical:
 	call GetBattleVar
 	bit SUBSTATUS_FOCUS_ENERGY, a
 	jr z, .CheckCritical
-; ALWAYS crit when high-crit + Focus Energy
-	ld a, BATTLE_VARS_MOVE_ANIM
-	call GetBattleVar
-	ld de, 1
-	ld hl, CriticalHitMoves
-	push bc
-	call IsInArray
-	pop bc
-	inc c ; +1 critical level, if not guaranteed crit
-	jr nc, .ScopeLens
-	ld a, 1
-	ld [wCriticalHit], a
-	ret
+
+; +1 critical level
+	inc c
 
 .CheckCritical:
 	ld a, BATTLE_VARS_MOVE_ANIM
