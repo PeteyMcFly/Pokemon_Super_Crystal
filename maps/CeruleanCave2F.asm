0bb1af9ea21b530b40d635d8b50e938c4b274f56
peteymcfly <talenmail@gmail.com>
2023-10-18 21:59:47 -0600

    added agatha and giovanni encounters. Set Giovanni to only appear after you defeat BLUE, and disappear after you've fought Mewtwo

--- maps/CeruleanCave2F.asm
+++ maps/CeruleanCave2F.asm
@@ -1,5 +1,6 @@
 	object_const_def ; object_event constants
 	const CERULEAN_CAVE_MEWTWO
+	const CERULEAN_CAVE_GIOVANNI
 
 CeruleanCave2F_MapScripts:
 	db 0 ; scene scripts
@@ -20,11 +21,11 @@ CeruleanCave2F_MapScripts:
 	disappear CERULEAN_CAVE_MEWTWO
 	return
 
-CeruleanCaveMewtwo:
+Mewtwo:
 	faceplayer
 	opentext
-	writetext CeruleanCaveMewtwoText
-	cry MEWTWO
+	writetext MewtwoText
+	cry Mewtwo
 	pause 15
 	closetext
 	setevent EVENT_FOUGHT_MEWTWO
@@ -35,8 +36,63 @@ CeruleanCaveMewtwo:
 	reloadmapafterbattle
 	end
 
-CeruleanCaveMewtwoText:
+MewtwoText:
 	text "... !?"
+	done
+
+TrainerExileGiovanni:
+	trainer EXILE, GIOVANNI, EVENT_BEAT_EXILE_GIOVANNI, ExileGiovanniSeenText, ExileGiovanniBeatenText, 0, .Script
+
+.Script:
+	checkevent EVENT_BEAT_BLUE
+	iftrue .Appear
+	sjump .NoAppear
+
+.Appear:
+	checkevent EVENT_FOUGHT_MEWTWO
+	iftrue .NoAppear
+	appear CERULEAN_CAVE_GIOVANNI
+	return
+	endifjustbattled
+	opentext
+	writetext ExileGiovanniAfterBattleText
+	waitbutton
+	closetext
+	end
+
+.NoAppear:
+	disappear CERULEAN_CAVE_GIOVANNI
+	return
+
+ExileGiovanniSeenText:
+	text "Hello there,"
+	line "#MON trainer."
+
+	para "I just came"
+	line "here to release"
+
+	para "a #MON"
+	line "into the wild."
+	done
+
+ExileGiovanniBeatenText:
+	text "Unbelievable."
+	line "You're like RED."
+	done
+
+ExileGiovanniAfterBattleText:
+	text "I was once the"
+	line "head boss of "
+	cont "TEAM ROCKET."
+
+	para "But I gave that"
+	line "that up long ago."
+
+	para "#MON are not"
+	line "tools for war."
+
+	para "Farewell,"
+	line "trainer."
 	done
 
 CeruleanCave2F_MapEvents:
@@ -49,5 +105,7 @@ CeruleanCave2F_MapEvents:
 
 	db 0 ; bg events
 
-	db 1 ; object events
-	object_event 4,  9, SPRITE_RHYDON, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, CeruleanCaveMewtwo, EVENT_CERULEAN_CAVE_MEWTWO_MEWTWO
+	db 2 ; object events
+	object_event 4,  9, SPRITE_RHYDON, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Mewtwo, EVENT_CERULEAN_CAVE_MEWTWO_MEWTWO
+	object_event 2,  4, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 5, TrainerExileGiovanni, -1
+
